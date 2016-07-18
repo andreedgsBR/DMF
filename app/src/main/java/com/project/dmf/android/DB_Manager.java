84f9792b79b34b3748e1ce0ae0088e07ecca4887
André Eduardo <andre.edgs@gmail.com>
2016-07-18 16:20:17 -0300

    Array para criar a fila no banco

--- app/src/main/java/com/project/dmf/android/DB_Manager.java
+++ app/src/main/java/com/project/dmf/android/DB_Manager.java
@@ -1,7 +1,11 @@
 package com.project.dmf.android;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by André Eduardo on 18/07/2016.
@@ -19,5 +23,22 @@ public class DB_Manager {
         String sql = "INSERT INTO fila (dados) VALUES ("+dados+")";
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         db.execSQL(sql);
+    }
+
+    public ArrayList<String> getAllItens(){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String sql = "SELECT *FROM fila";
+        Cursor cursor = db.rawQuery(sql, null);
+        ArrayList<String> dados = null;
+
+        if(cursor != null && cursor.moveToFirst()){
+            dados = new ArrayList<String>();
+
+            do {
+                dados.add(cursor.getString(1));
+            } while(cursor.moveToNext());
+        }
+
+        return dados;
     }
 }
